3be02ac97d5bba79dec11ae8b512e8049afbaf91
MizJones <41445506+MizJones@users.noreply.github.com>
2018-07-22 22:09:22 -0700

    Add files via upload
    Second Commit created game logic

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king1", "king2"];
+var cardsInPlay = [];
+var cardOne = cards[cards.length - 4];
+var cardTwo = cards[cards.length - 3];
+var cardThree;
+var cardFour
+cardsInPlay.push(cardOne)
+cardsInPlay.push(cardTwo)
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("Player picked" + " " + cardOne + " " + "and then" + " " + cardTwo);
+
+  if (cardsInPlay.length === 2) {
+    alert("Yinz picked two!");
+  } else {
+    alert("Ya Jagoff! Can't you cahhhnt?");
+  }
+
+
+  if (cardOne === cardTwo) {
+    alert("Ya gotta match! Rill Nice n'at!");
+  } else {
+    alert("No deal, Bucky. Try again.");
+  }
+
